06bda59ca99627b746f8e007eefadd934f1cbc1c
hi20160616 <hi20160616@163.com>
2021-09-03 11:58:17 +0800

    go build constraints fix

--- configs/linux.go
+++ configs/linux.go
@@ -1,19 +1,19 @@
 // +build !windows
 
-package configs
-
-import (
-	"os"
-	"strings"
-)
-
-func setRootPath(cfg *Config) *Config {
-	cfg.RootPath, cfg.Err = os.Getwd()
-	if cfg.Err != nil {
-		return cfg
-	}
-	if strings.Contains(os.Args[0], ".test") {
-		return rootPath4Test(cfg)
-	}
-	return cfg
-}
+// package configs
+//
+// import (
+//         "os"
+//         "strings"
+// )
+//
+// func setRootPath(cfg *Config) *Config {
+//         cfg.RootPath, cfg.Err = os.Getwd()
+//         if cfg.Err != nil {
+//                 return cfg
+//         }
+//         if strings.Contains(os.Args[0], ".test") {
+//                 return rootPath4Test(cfg)
+//         }
+//         return cfg
+// }
